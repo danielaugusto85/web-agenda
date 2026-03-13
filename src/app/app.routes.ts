3360a2d9efa15563da9053451e3a66147f708523
danielaugusto85 <danielmiceli@gmail.com>
2026-03-13 13:18:13 -0300

    Desenvolvimento do layout e cadastro de categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { AutenticarUsuario } from './pages/autenticar-usuario/autenticar-usuario
 import { CadastrarUsuario } from './pages/cadastrar-usuario/cadastrar-usuario';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { AuthGuard } from './guards/auth.guard';
+import { CadastrarCategoria } from './pages/cadastrar-categoria/cadastrar-categoria';
+import { ConsultarCategoria } from './pages/consultar-categoria/consultar-categoria';
+import { EditarCategoria } from './pages/editar-categoria/editar-categoria';
 
 export const routes: Routes = [
     {
@@ -16,7 +19,22 @@ export const routes: Routes = [
     },
     {
         path : "pages/cadastrar-usuario", //rota de navegação
-        component: CadastrarUsuario //classe do componente
+        component: CadastrarUsuario, //classe do componente
+    },
+    {
+        path : "pages/cadastrar-categoria", //rota de navegação
+        component: CadastrarCategoria, //classe do componente
+        canActivate: [AuthGuard] //aplicando o guardião
+    },
+    {
+        path : "pages/consultar-categoria", //rota de navegação
+        component: ConsultarCategoria, //classe do componente
+        canActivate: [AuthGuard] //aplicando o guardião
+    },
+    {
+        path : "pages/editar-categoria", //rota de navegação
+        component: EditarCategoria, //classe do componente
+        canActivate: [AuthGuard] //aplicando o guardião
     },
     {
         path : "", //rota de navegação para a página inicial
